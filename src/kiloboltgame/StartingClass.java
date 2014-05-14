7ebd3f8f7e26e41f9804a480225badad0839c534
willbush <william.bush@utdallas.edu>
2014-05-14 03:49:20 -0500

    finished Day 3: Taking User Input

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -3,14 +3,17 @@ package kiloboltgame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class StartingClass extends Applet implements Runnable {
+public class StartingClass extends Applet implements Runnable, KeyListener {
 
     @Override
     public void init() {
         setSize(800, 400);
         setBackground(Color.black);
         setFocusable(true);
+        addKeyListener(this);
         Frame frame = (Frame) this.getParent().getParent();
         frame.setTitle("Q-Bot Alpha");
 
@@ -44,6 +47,36 @@ public class StartingClass extends Applet implements Runnable {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        getKeyEvent(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        getKeyEvent(e);
+    }
+
+    private void getKeyEvent(KeyEvent e) {
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_UP:
+            break;
+        case KeyEvent.VK_DOWN:
+            break;
+        case KeyEvent.VK_LEFT:
+            break;
+        case KeyEvent.VK_RIGHT:
+            break;
+        case KeyEvent.VK_SPACE:
+            break;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
 
     }
 }
