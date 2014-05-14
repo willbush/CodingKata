87713024c32dbfa53674f26cc4111a1a1f2d4c10
willbush <william.bush@utdallas.edu>
2014-05-14 03:17:35 -0500

    finished Day2: Basic Framework

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -1,19 +1,25 @@
 package kiloboltgame;
 
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
 
-public class StartingClass extends Applet {
+public class StartingClass extends Applet implements Runnable {
 
     @Override
     public void init() {
-        // TODO Auto-generated method stub
-        super.init();
+        setSize(800, 400);
+        setBackground(Color.black);
+        setFocusable(true);
+        Frame frame = (Frame) this.getParent().getParent();
+        frame.setTitle("Q-Bot Alpha");
+
     }
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
-        super.start();
+        Thread thread = new Thread(this);
+        thread.start();
     }
 
     @Override
@@ -26,5 +32,18 @@ public class StartingClass extends Applet {
     public void destroy() {
         // TODO Auto-generated method stub
         super.destroy();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            repaint();
+            try {
+                Thread.sleep(17);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
