500bb6af466a9c7370e98c46ba3d4b7ec25aa7a4
willbush <william.bush@utdallas.edu>
2014-05-20 13:41:48 -0500

    finished unit 4, added basic enemy AI

--- src/kiloboltgame/Enemy.java
+++ src/kiloboltgame/Enemy.java
@@ -3,19 +3,34 @@ package kiloboltgame;
 import java.awt.Rectangle;
 
 public class Enemy {
-    
+
     public Rectangle enemyCollisionBox = new Rectangle(0, 0, 0, 0);
 
-    private int enemySpeedX, enemyPosX, enemyPosY;
+    private int enemySpeedX, enemyPosX, enemyPosY, movementSpeed;
     private Background bg = StartingClass.getBg1();
+    private Robot robot = StartingClass.getRobot();
 
     public void update() {
+        follow();
         enemyPosX += enemySpeedX;
-        enemySpeedX = bg.getbGspeedX() * 5;
+        enemySpeedX = bg.getbGspeedX() * 5 + movementSpeed;
         enemyCollisionBox.setBounds(enemyPosX - 25, enemyPosY - 25, 50, 60);
 
         if (enemyCollisionBox.intersects(Robot.checkCollisionBox)) {
             checkCollision();
+        }
+    }
+
+    private void follow() {
+        if (enemyPosX < -95 || enemyPosX > 810)
+            movementSpeed = 0;
+        else if (Math.abs(robot.getRobotPosX() - enemyPosX) < 5)
+            movementSpeed = 0;
+        else {
+            if (robot.getRobotPosX() >= enemyPosX)
+                movementSpeed = 1;
+            else
+                movementSpeed = -1;
         }
     }
 
