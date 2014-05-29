70c7f836ad3ba3c7c180273eea05b481a79d936b
willbush <william.bush@utdallas.edu>
2014-05-28 19:32:56 -0500

    added Scrollable super class and its subclasses.

---
+++ core/src/com/kilobolt/GameObjects/Grass.java
@@ -0,0 +1,9 @@
+package com.kilobolt.GameObjects;
+
+public class Grass extends Scrollable {
+
+    public Grass(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+    }
+
+}
