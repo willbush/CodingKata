877630e705b8ce41ac577c3104542ef4d2ed1b0c
William Bush <william.bush@utdallas.edu>
2014-08-18 18:44:46 -0500

    add basic MVC structure using repository pattern

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Ninject;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure {
 	public class NinjectDependencyResolver : IDependencyResolver {
@@ -21,7 +25,15 @@ namespace SportsStore.WebUI.Infrastructure {
 		}
 
 		private void AddBindings() {
-			// add bindings here
+			Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+
+			mock.Setup(m => m.Products).Returns(new List<Product> {
+				new Product { Name = "Football", Price = 25 },
+				new Product { Name = "Surf Board", Price = 179 },
+				new Product { Name = "Running Shoes", Price = 95 }
+			});
+
+			kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
 		}
 	}
 }
