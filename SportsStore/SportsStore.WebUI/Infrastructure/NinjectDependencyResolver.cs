65eeae7d8ae57a25bc50cc0bad4ab7490db74ced
William Bush <william.bush@utdallas.edu>
2014-08-19 09:04:09 -0500

    remove mock and add real repository binding

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using Ninject;
 using Moq;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure {
@@ -25,15 +26,7 @@ namespace SportsStore.WebUI.Infrastructure {
 		}
 
 		private void AddBindings() {
-			Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
-
-			mock.Setup(m => m.Products).Returns(new List<Product> {
-				new Product { Name = "Football", Price = 25 },
-				new Product { Name = "Surf Board", Price = 179 },
-				new Product { Name = "Running Shoes", Price = 95 }
-			});
-
-			kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+			kernel.Bind<IProductsRepository>().To<EFProductRepository>();
 		}
 	}
 }
