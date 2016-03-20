6ee458a2c41fbc7aa72fcd565f96c60427f78191
willbush <william.bush@utdallas.edu>
2016-03-20 01:02:58 -0500

    add bowling game kata implemented in haskell

---
+++ BowlingGame/BowlingGame.hs
@@ -0,0 +1,21 @@
+-- My haskell implementation of Uncle Bob's Bowling Game kata.
+-- It just consist of a function that scores a bowling game.
+-- it assumes the given list of rolls represent a valid game.
+
+module BowlingGame where
+
+scoreGame :: [Int] -> Int
+scoreGame xs = go xs 0 (0 :: Int) where
+  go _ score 10 = score
+  go rolls@(firstInFrame:x:y:zs) score frameCount
+    | isStrike firstInFrame  = go (drop 1 rolls)  (score + firstInFrame + x + y) (frameCount + 1)
+    | isSpare firstInFrame x = go (y:zs)          (score + firstInFrame + x + y) (frameCount + 1)
+    | otherwise              = go (drop 2 rolls)  (score + firstInFrame + x)     (frameCount + 1)
+  go (firstInFrame:x:ys) score frameCount = go ys (score + firstInFrame + x)     (frameCount + 1)
+  go _ score _ = score
+
+isSpare :: Int -> Int -> Bool
+isSpare firstInFrame secondInFrame = firstInFrame + secondInFrame == 10
+
+isStrike :: Int -> Bool
+isStrike roll = roll == 10
---
+++ BowlingGame/BowlingGameSpec.hs
@@ -0,0 +1,34 @@
+module BowlingGameSpec where
+
+import BowlingGame
+import Test.Hspec
+
+strike :: Int
+strike = 10
+
+maxNumOfRolls :: Int
+maxNumOfRolls = 20
+
+main :: IO ()
+main = hspec $
+  describe "Bowling Game" $
+    describe "score method" $ do
+      it "returns a score of 0 when given a gutter game" $ do
+        scoreGame [] `shouldBe` (0 :: Int)
+        let gutterBall = 0 :: Int
+        scoreGame (replicate maxNumOfRolls gutterBall) `shouldBe` 0
+      it "returns a score of 20 when given a game of all ones" $
+        scoreGame (replicate maxNumOfRolls 1) `shouldBe` (20 :: Int)
+      it "can score a single spare followed by gutter balls" $ do
+        let rollsRemaining = 17 :: Int
+        scoreGame (5 : 5 : 3 : replicate rollsRemaining 0) `shouldBe` (16 :: Int)
+      it "can score a single strike followed by gutter balls" $ do
+        let rollsRemaining = 16 :: Int
+        scoreGame (strike : 3 : 4 : replicate rollsRemaining 0) `shouldBe` (24 :: Int)
+      it "can score a perfect game" $ do
+        let rollsRemaining = 12 :: Int
+        scoreGame (replicate rollsRemaining strike) `shouldBe` (300 :: Int)
+      it "can score some acceptance tests" $ do
+        scoreGame [5,2,3,4,7,3,1,9,10,5,3,10,8,2,6,0,8,2,7] `shouldBe` (130 :: Int)
+        scoreGame [5,2,3,4,7,3,8,2,10,5,3,10,8,2,6,0,8,2,7] `shouldBe` (137 :: Int)
+        scoreGame [10,10,8,0,10,10,10,10,7,0,10,10,10,10] `shouldBe` (225 :: Int)
