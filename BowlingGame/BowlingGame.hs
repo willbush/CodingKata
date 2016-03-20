24e47cd4dc198bda4aa59559f364ba75063b7951
willbush <william.bush@utdallas.edu>
2016-03-20 01:34:28 -0500

    minor refactor to bowling game.

--- BowlingGame/BowlingGame.hs
+++ BowlingGame/BowlingGame.hs
@@ -7,9 +7,9 @@ module BowlingGame where
 scoreGame :: [Int] -> Int
 scoreGame xs = go xs 0 (0 :: Int) where
   go _ score 10 = score
-  go rolls@(firstInFrame:x:y:zs) score frameCount
+  go rolls@(firstInFrame:x:y:_) score frameCount
     | isStrike firstInFrame  = go (drop 1 rolls)  (score + firstInFrame + x + y) (frameCount + 1)
-    | isSpare firstInFrame x = go (y:zs)          (score + firstInFrame + x + y) (frameCount + 1)
+    | isSpare firstInFrame x = go (drop 2 rolls)  (score + firstInFrame + x + y) (frameCount + 1)
     | otherwise              = go (drop 2 rolls)  (score + firstInFrame + x)     (frameCount + 1)
   go (firstInFrame:x:ys) score frameCount = go ys (score + firstInFrame + x)     (frameCount + 1)
   go _ score _ = score
